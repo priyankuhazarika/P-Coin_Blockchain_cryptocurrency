f01e19972c8a813d3e9907f3893cc931bcf1dd78
Priyanku <35552170+Priyankuabi@users.noreply.github.com>
2019-03-12 22:30:59 +0530

    Add files via upload

--- block.py
+++ block.py
@@ -1,9 +1,13 @@
 from time import time
 
-class Block:
+from utility.printable import Printable
+
+class Block(Printable):
     def __init__(self, index, previous_hash, transactions, proof, time=time()):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = time
         self.transactions = transactions
         self.proof = proof
+
+    
